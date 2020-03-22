108bbffc16fedbc52d50f3c84526937667df279d
Neil Vaytet <neil.vaytet@esss.se>
2020-03-22 21:56:58 +0100

    progress on plot2d

--- src/sciview/plot_2d.py
+++ src/sciview/plot_2d.py
@@ -31,6 +31,11 @@ def plot_2d(x, y, data, cmap="viridis", vmin=None, vmax=None):
     scale_x = dx / (np.amax(xe) - xmin)
     scale_y = dy / (np.amax(ye) - ymin)
 
+
+
+
+
+
     # x_grid, y_grid = np.meshgrid((x - xmin) * scale_x, (y - ymin) * scale_y, indexing="ij")
     y_grid, x_grid = np.meshgrid((y - ymin) * scale_y, (x - xmin) * scale_x, indexing="ij")
 
@@ -41,24 +46,105 @@ def plot_2d(x, y, data, cmap="viridis", vmin=None, vmax=None):
     pts[:, 0] = x_grid.flatten()
     pts[:, 1] = y_grid.flatten()
 
+
+
+
+
+
     # pts = p3.BufferAttribute(array=det_pos)
 
     scalar_map = get_cmap(data=None, cmap="viridis", log=False, vmin=None, vmax=None)
-    # colors = scalar_map.to_rgba(data).astype(np.float32)
+    colors = scalar_map.to_rgba(data.ravel()).astype(np.float32)
 
     # colors = p3.BufferAttribute(
     #     array=scalar_map.to_rgba(data).astype(np.float32))
-    geometry = p3.BufferGeometry(attributes={
-        'position': p3.BufferAttribute(array=pts),
-        'color': p3.BufferAttribute(
-            array=scalar_map.to_rgba(data).astype(np.float32))
-    })
-    material = p3.PointsMaterial(vertexColors='VertexColors',
-                                           size=pixel_size_x)
-                                           # map=texture,
-                                           # depthTest=False,
-                                           # transparent=True)
-    image = p3.Points(geometry=geometry, material=material)
+
+    nx = xe.shape[0] - 1
+    ny = ye.shape[0] - 1
+    npixels = nx * ny
+
+
+    arr_x = np.zeros(2 * nx, dtype=np.float32)
+    arr_x[::2] = xe[:-1]
+    arr_x[1::2] = xe[1:]
+    arr_y = np.zeros(2 * ny, dtype=np.float32)
+    arr_y[::2] = ye[:-1]
+    arr_y[1::2] = ye[1:]
+
+    # x_grid, y_grid = np.meshgrid((arr_x - xmin) * scale_x, (arr_y - ymin) * scale_y)
+    x_grid, y_grid = np.meshgrid(arr_x, arr_y)
+
+    # print(arr_x.dtype)
+    # print(x_grid.dtype, y_grid.dtype)
+    # print(x_grid.ravel().dtype)
+    vertices = np.array([x_grid.ravel(), y_grid.ravel(), np.zeros(x_grid.size, dtype=np.float32)]).T
+
+    face_elem = np.array([[0, 1, nx*2], [1, nx*2+1, nx*2]], dtype=np.uint32)
+
+    # faces = np.tile
+
+    # self.vertices = np.tile(detector_shape, [self.ndets, 1]) + np.repeat(self.det_pos, self.nverts, axis=0)
+
+    # faces = np.arange(self.nverts * self.ndets, dtype=np.uint)
+    faces = np.tile(face_elem, [npixels, 1]) + \
+            np.repeat(np.arange(0, npixels*2, 2, dtype=np.uint32), 2*3, axis=0).reshape(npixels*2, 3) + \
+            np.repeat(np.arange(0, (nx*2)*ny, nx*2, dtype=np.uint32), (nx*2)*3, axis=0).reshape(npixels*2, 3)
+
+
+    # faces = np.tile(detector_faces, [self.ndets, 1]) + np.repeat(
+    #     np.arange(0, self.ndets*self.nverts, self.nverts, dtype=np.uint32), self.nfaces*3, axis=0).reshape(self.nfaces*self.ndets, 3)
+
+    vertexcolors = np.repeat(colors[:, :3], 4, axis=0).astype(np.float32)
+    # vertexcolors = np.zeros([npixels*2, 3], dtype=np.float32)
+
+    print(vertices.shape)
+    print(faces.shape)
+    print(vertexcolors.shape)
+    print(vertices.dtype)
+    print(faces.dtype)
+    print(vertexcolors.dtype)
+
+    print("vertices")
+    print(vertices)
+    print("faces")
+    print(faces)
+    print("colors")
+    print(vertexcolors)
+
+
+    geometry = p3.BufferGeometry(attributes=dict(
+        position=p3.BufferAttribute(vertices, normalized=False),
+        index=p3.BufferAttribute(faces.ravel(), normalized=False),
+        color=p3.BufferAttribute(vertexcolors),
+    ))
+
+
+    material = p3.MeshBasicMaterial(vertexColors='VertexColors')
+    # material = p3.MeshBasicMaterial()
+
+    # self.material = self.p3.MeshPhongMaterial(vertexColors='VertexColors',
+    #                                           transparent=True)
+
+
+    image = p3.Mesh(geometry=geometry, material=material)
+
+
+
+
+
+
+
+    # geometry = p3.BufferGeometry(attributes={
+    #     'position': p3.BufferAttribute(array=pts),
+    #     'color': p3.BufferAttribute(
+    #         array=scalar_map.to_rgba(data).astype(np.float32))
+    # })
+    # material = p3.PointsMaterial(vertexColors='VertexColors',
+    #                                        size=pixel_size_x)
+    #                                        # map=texture,
+    #                                        # depthTest=False,
+    #                                        # transparent=True)
+    # image = p3.Points(geometry=geometry, material=material)
 
 
 
@@ -75,12 +161,12 @@ def plot_2d(x, y, data, cmap="viridis", vmin=None, vmax=None):
     # # height= 500
 
     # # Create the threejs scene with ambient light and camera
-    # # camera = p3.PerspectiveCamera(position=[0.5*dx, 0.5*dy, 0],
-    #                                         aspect=dx / dy)
-    camera = p3.OrthographicCamera(0, dx, 0, dy, -0.5*dx, 0.5*dx)
+    camera = p3.PerspectiveCamera(position=[0, 0, 10],
+                                            aspect=dx / dy)
+    # camera = p3.OrthographicCamera(0, dx, 0, dy, -0.5*dx, 0.5*dx)
 
     return render(objects=image, camera=camera,
-                  enableRotate=False, width=dx, height=dy)
+                  enableRotate=True, width=dx, height=dy)
 
     # # key_light = p3.DirectionalLight(position=[0, 10, 10])
     # # ambient_light = p3.AmbientLight()
